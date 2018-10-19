ddc2bc806f27cd0850c1af3051fefbaf5cb39e6a
Pulkit Goel <pulkitgoel234@gmail.com>
2018-10-20 03:28:13 +0530

    Stage 1

---
+++ Reciever/Reciever.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>             
+
+SoftwareSerial GSerial(11,12);               //Created instance for LiFi
+char rec=0;
+
+void setup()
+{
+  Serial.begin(9600);                       //Baud rate for Serial port      
+  GSerial.begin(400);                       //Baud rate for LiFi
+}
+
+void loop()
+{
+  if(GSerial.available() != 0)
+  {    
+    rec = GSerial.read();
+    Serial.print(rec);
+  }
+}
---
+++ Transmitter/Transmitter.ino
@@ -0,0 +1,39 @@
+#include <Keypad.h>
+#include <SoftwareSerial.h>
+
+const byte ROWS = 4;
+const byte COLS = 4;
+char customKey;
+
+char hexaKeys[ROWS][COLS] = {       //Keypad definiton
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {11,12,A0,A1}; 
+byte colPins[COLS] = {A2,A3,A4,A5}; 
+SoftwareSerial GSerial(2,3);                            //LiFi serial instance
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+char keycount=0;
+char code[5];
+
+void setup() 
+{
+  delay(1000);
+  Serial.begin(9600);         //Set Baud rate
+  Serial.println("Keyboard Test:");
+  GSerial.begin(400);         //Set Transfer rate for LiFi
+  GSerial.print('&');         
+}
+
+void loop() 
+{    
+  customKey = customKeypad.getKey();
+  if(customKey && (customKey !='='))
+  {      
+      Serial.print(customKey);
+      GSerial.print(customKey);
+  }  
+}
